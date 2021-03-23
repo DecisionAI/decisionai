ae2ffaabeda59fa855aa667b34331c659a382407
Dan Becker <dansbecker@gmail.com>
2021-03-23 17:32:23 -0600

    fix install issue

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 import setuptools
+import decisionai
 
 setuptools.setup(
         name="decisionai",
         url="https://github.com/decisionai/decisionai",
         packages=setuptools.find_packages(),
-        python_requires='~=3.7',
+        version=decisionai.__version__,
+        python_requires='>=3.7',
 )
